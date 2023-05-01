e3a0dff0583179d0a61c349497de5f432ca35566
Farin Lippmann <toucanmeister@gmail.com>
2023-05-01 09:55:28 +0200

    added vis ex03 tasks

---
+++ Semester 2/Visualisierung/ex03/code/task3_1.py
@@ -0,0 +1,17 @@
+import numpy as np # math
+import matplotlib.pyplot as plt # plotting
+import matplotlib.image as mpimg # loading images
+import pandas # handling csv data
+
+fig = plt.figure(figsize=(8,7))
+
+# loading a table with pandas
+data_frame = pandas.read_csv("spread_data.csv")
+
+
+
+
+
+
+# Run show, to make sure everything is displayed.
+plt.show()
