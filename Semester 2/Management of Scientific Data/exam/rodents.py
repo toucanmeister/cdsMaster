27c982b57a9be864364cbcfe74365501f7fa9c8a
Farin Lippmann <toucanmeister@gmail.com>
2023-07-11 11:35:11 +0200

    changes to mosd exam plot

--- Semester 2/Management of Scientific Data/exam/rodents.py
+++ Semester 2/Management of Scientific Data/exam/rodents.py
@@ -27,9 +27,12 @@ df = df[df['City'] == 'brooklyn']
 
 ## Plot
 month_counts = df.groupby('week').count()
-ax = month_counts.plot.line()
+fig, ax = plt.subplots(1, 1)
+ax.plot(month_counts, linewidth=1.5)
 ax.set_xlabel('week')
 ax.set_ylabel('311 calls about rodents')
-ax.set_xticks([327 / 7, 358 / 7])
-ax.set_xticklabels(['Thanksgiving', 'Christmas'])
+ax.set_xticks([327 / 7])
+ax.set_xticklabels(['Thanksgiving',])
+ax.axvline(x=327 / 7, color='black', linewidth=0.5)
+ax.axvspan(327/7, (327+14)/7, color='black', alpha=0.1, lw=0)
 plt.show()
